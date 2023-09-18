a987c4fa5101fc771d1e2166fa52c362cd7dd244
Andrew Trotter <andrewjtrotter@gmail.com>
2023-09-18 16:53:32 +1200

    organised public folder

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
 import Head from 'next/head';
+import Header from '../componenets/Header';
 import styles from '../styles/Home.module.css';
+import { Typography } from '@mui/material'
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Home - WASSP Multimbeam</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1>Hello world</h1>
+        <Header />
+        <Typography variant='h1'>Hello world</Typography>
+        
       </main>
 
       <footer>
