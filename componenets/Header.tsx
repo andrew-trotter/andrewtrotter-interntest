3136fe4fab2d07b599389fa3c302e536bcfa33ad
Andrew Trotter <andrewjtrotter@gmail.com>
2023-09-20 17:32:19 +1200

    basic setup for header. have to right align elements, add arrow for EN and remove EN for mobile

---
+++ componenets/Header.tsx
@@ -0,0 +1,37 @@
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  IconButton,
+  MenuItem,
+  Typography,
+  Box,
+} from '@mui/material'
+import Menu from '../public/svg/Menu.svg'
+import WasspLogo from '../public/svg/Logo.svg'
+import Image from 'next/image'
+
+const links = ['SEARCH', 'LOGIN', 'EN']
+export default function Header() {
+  return (
+    <AppBar position="static" color="transparent">
+      <Container sx={{margin:"0"}}>
+        <Toolbar disableGutters>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Image src={WasspLogo} alt="Wassp Logo"></Image>
+            <Box sx={{ display: 'flex', flexGrow: 1 }}>
+              {links.map((link: string) => (
+                <MenuItem key={link}>
+                  <Typography> {link} </Typography>
+                </MenuItem>
+              ))}
+              <IconButton>
+                <Image src={Menu} alt="menu icon"></Image>
+              </IconButton>
+            </Box>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
+}
