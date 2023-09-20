6417f6cbf12dc76c621c3f421577957dc346f91f
Andrew Trotter <andrewjtrotter@gmail.com>
2023-09-20 17:33:28 +1200

    made for adding typekit font

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* <link rel="stylesheet" href="https://use.typekit.net/ztw0vem.css" /> */}
+      </Head>
+      <main>
+        <Main />
+        <NextScript />
+      </main>
+    </Html>
+  )
+}
