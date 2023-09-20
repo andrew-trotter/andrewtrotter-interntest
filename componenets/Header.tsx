9ede49ed109be07b7be9a3bcb46e3419d4d4feec
Andrew Trotter <andrewjtrotter@gmail.com>
2023-09-20 20:33:47 +1200

    style: header collapse complete

--- componenets/Header.tsx
+++ componenets/Header.tsx
@@ -5,36 +5,42 @@ import {
   IconButton,
   MenuItem,
   Typography,
+  Button,
   Box,
 } from '@mui/material'
 import Menu from '../public/svg/Menu.svg'
 import WasspLogo from '../public/svg/Logo.svg'
 import ArrowDown from '../public/svg/ArrowDown.svg'
 import Image from 'next/image'
+import React from "react"
 
 const links = ['SEARCH', 'LOGIN', 'EN']
 export default function Header() {
   return (
     <AppBar position="static" color="transparent">
-      <Container sx={{margin:"0"}}>
+      <Container maxWidth={false} sx={{marginTop:'3em'}} >
         <Toolbar disableGutters>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Box sx={{flexGrow: '1'}}>
             <Image src={WasspLogo} alt="Wassp Logo"></Image>
-            <Box sx={{ display: 'flex', flexGrow: 1 }}>
-              {links.map((link: string) => (
-                <MenuItem key={link}>
-                  <Typography>{link}</Typography>
-                  {link === "EN" &&
-                    <Box sx={{marginLeft:'0.5em'}}>
-                      <Image src={ArrowDown} alt='language drop arrow' ></Image>
+          </Box>
+          <Box sx={{ display: 'flex', gap: "2em", justifySelf: 'end' }}>
+            {links.map((link: string) => (
+              <React.Fragment key={link}>                
+                  {link === 'EN' ? (
+                    <Box sx={{ gap:"1em", display:{xs:'none', sm:'flex'}}}>
+                      <Typography>{link}</Typography>
+                      <Image src={ArrowDown} alt="language down arrow"></Image>
                     </Box>
-                  }
-                </MenuItem>
-              ))}
-              <IconButton>
-                <Image src={Menu} alt="menu icon"></Image>
-              </IconButton>
-            </Box>
+                  ) : (
+                    <Box>
+                      <Typography>{link}</Typography>
+                    </Box>
+                  )}                
+              </React.Fragment>
+            ))}
+            <IconButton>
+              <Image src={Menu} alt="menu icon"></Image>
+            </IconButton>
           </Box>
         </Toolbar>
       </Container>
